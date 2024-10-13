cf00ce2ad65245de0901482e2e090c3dcbd9fc6a
Joyo Nurdiansyah <nurdiansyahjoyo@gmail.com>
2024-10-13 13:13:19 +0700

    create product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -20,9 +21,12 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::all();
+        return view('admin.products.create', [
+            'categories' => $categories,
+        ]);
     }
-
+    
     /**
      * Store a newly created resource in storage.
      */
--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -1,1 +1,100 @@
-{{-- file ini digunakan sebagai form menambahkan data category --}}
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('New Product') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden p-10 shadow-sm sm:rounded-lg">
+
+                @if($errors->any())
+                    @foreach ($errors->all() as $error)
+                        <div class="py-3 w-full rounded-3xl bg-red-500 text-white p-4 mb-2">
+                            {{$error}}
+                        </div>
+                    @endforeach
+                @endif
+
+                <form method="POST" action="{{ route('admin.products.store') }}" enctype="multipart/form-data">
+                    @csrf
+
+                    <div>
+                        <x-input-label for="name" :value="__('Name')" />
+                        <x-text-input id="name" class="block mt-1 w-full" type="text" name="name"
+                            :value="old('name')" required autofocus autocomplete="name" />
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    </div>
+
+                    <div class="mt-4">
+                        <x-input-label for="price" :value="__('price')" />
+                        <x-text-input id="price" class="block mt-1 w-full" type="number" name="price"
+                            :value="old('price')" required autofocus autocomplete="price" />
+                        <x-input-error :messages="$errors->get('price')" class="mt-2" />
+                    </div>
+
+                    <div class="mt-4">
+                        <x-input-label for="category" :value="__('category')" />
+                        <select name="category_id" id="category_id" class="py-3 rounded-lg pl-3 w-full border border-slate-300">
+                            <option value="">Pilih Kategori Produk</option>
+
+                            @forelse ($categories as $category)
+                                <option value="{{$category->id}}">{{$category->name}}</option>
+                            @empty
+                            @endforelse
+                        </select>
+                        <x-input-error :messages="$errors->get('category')" class="mt-2" />
+                    </div>
+
+                    <div class="mt-4">
+                        <x-input-label for="about" :value="__('about')" />
+                        <textarea name="about" id="about" cols="30" rows="5" class="border border-slate-300 rounded-xl w-full"></textarea>
+                        <x-input-error :messages="$errors->get('about')" class="mt-2" />
+                    </div>
+
+                    <!-- Icon Upload Input -->
+                    <div class="mt-4">
+                        <x-input-label for="photo" :value="__('photo')" />
+                        <x-text-input id="photo" class="block mt-1 w-full" type="file" name="photo"
+                            :value="old('photo')" required autofocus autocomplete="photo" onchange="previewFile()" />
+                        <x-input-error :messages="$errors->get('photo')" class="mt-2" />
+                    </div>
+
+                    <!-- Preview of Uploaded File -->
+                    <div class="mt-4">
+                        <img id="file-preview" class="mt-2" style="max-width: 300px; display: none;" />
+                    </div>
+
+                    <div class="flex items-center justify-end mt-4">
+                        <x-primary-button class="ms-4">
+                            {{ __('Add New Product') }}
+                        </x-primary-button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+    <script>
+        function previewFile() {
+            const file = document.getElementById('photo').files[0]; 
+            const preview = document.getElementById('file-preview');
+
+            if (file) {
+                const reader = new FileReader();
+
+                reader.onload = function(e) {
+                    preview.src = e.target.result;
+                    preview.style.display = 'block';
+                }
+
+                reader.readAsDataURL(file);
+            } else {
+                preview.style.display = 'none';
+            }
+        }
+    </script>
+
+</x-app-layout>
