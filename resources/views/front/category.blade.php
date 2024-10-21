347c5ddb9c14af04f12a0cef752b7147e37975a7
Joyo Nurdiansyah <nurdiansyahjoyo@gmail.com>
2024-10-21 15:18:36 +0700

    title category

--- resources/views/front/category.blade.php
+++ resources/views/front/category.blade.php
@@ -17,7 +17,7 @@
             <img src="{{ asset('assets/svgs/ic-arrow-left.svg') }}" class="size-5" alt="">
         </a>
         <p class="absolute text-base font-semibold translate-x-1/2 -translate-y-1/2 top-1/2 right-1/2">
-            Search Products
+            {{$category->name}} Products
         </p>
         <button type="button" class="p-2 bg-white rounded-full">
             <img src="{{ asset('assets/svgs/ic-triple-dots.svg') }}" class="size-5" alt="">
